21914baa12a6b7bf93ea618077035b118835c24b
Tamal Saha <tamal@appscode.com>
2023-11-13 11:58:48 -0800

    Use openapi-generator-cli:v7.1.0
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ go run main.go
 
 docker run --rm \
   --user $(id -u):$(id -g) \
-  -v $(PWD)/..:/local openapitools/openapi-generator-cli:v7.0.1 generate \
+  -v $(PWD)/..:/local openapitools/openapi-generator-cli:v7.1.0 generate \
   -i /local/james-openapi-schema/api.yaml \
   -g go \
   --git-user-id searchlight \
